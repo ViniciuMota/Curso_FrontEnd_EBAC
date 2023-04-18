16f3b377b428376112711771f55c5b8b97fd92db
ViniciuMota <Vinimora44@gmail.com>
2023-04-18 15:28:36 -0300

    Execicio de plugins jquery

--- main.js
+++ main.js
@@ -1,12 +1,50 @@
 $(document).ready(function(){
-    $('ul').on('click','li',function(){
-        $(this).toggleClass('riscado');
-    })
+    $('#telefone').mask('(00) 00000-0000', {
+        placeholder: '(00) 00000-0000'
+    });
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    });
+    $('#cep').mask('00000-000',{
+        placeholder: '00000-000'
+    });
 
-    $('form').on('submit', function(e){
-        e.preventDefault();
-        const notaTarefa = $('#nome-tarefa').val();
-        $(`<li>${notaTarefa}</li>`).appendTo('ul');
-        $('#nome-tarefa').val('');
+    $('form').validate({
+        rules:{
+            nome:{
+                required: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            telefone:{
+                required: true
+            },
+            cpf:{
+                required: true
+            },
+            endereco:{
+                required: true
+            },
+            cep:{
+                required: true
+            },
+            
+        },
+        messages:{
+            nome: 'Por favor insira o seu nome',
+            email:'Esse campo é obrigatorio',
+            telefone:'Esse campo é obrigatorio'
+        },
+        submitHandler: function(form){
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos){
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
     })
 })
