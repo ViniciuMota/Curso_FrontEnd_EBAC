d752a462877718bc98006d4aaf6df4ba31f56042
ViniciuMota <vinimota44@gmail.com>
2023-12-19 10:35:25 -0300

    exercicio ajax

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    const endpoint = 'https://api.github.com/users/ViniciuMota';
+    
+    fetch(endpoint)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(json){
+        console.log(json);
+        $('.profile-avatar').attr('src',json.avatar_url);
+        $('.profile-name').text(json.name);
+        $('.profile-username').text(`@${json.login}`);
+        $('.repositorios-item').text(json.public_repos)
+        $('.seguidores-item').text(json.followers)
+        $('.seguindo-item').text(json.following)
+        $('.profile-link').attr('href', json.html_url);
+    })
+})
