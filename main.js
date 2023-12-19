b01830d06f8c0a5fdcb915d9a991af04a71abaa5
ViniciuMota <vinimota44@gmail.com>
2023-12-19 16:58:38 -0300

    exercicio es6

--- main.js
+++ main.js
@@ -1,44 +1,15 @@
-// abstrata
-function Animal(raca, idade) {
-    this.raca = raca;
-    this.idade = idade;
-  
-    this.emitirSom = function(nome) {
-      console.log(`${nome} faz som`);
-    }
-  }
-  
-  // herdeiras
-  function Cachorro (raca, idade, nome) {
-    this.nome = nome;
-    Animal.call(this, raca, idade);
-      
-    this.latir = function(){
-        console.log("Au au!")
-    }
-  }
-  
-  function Gato (raca, idade, nome) {
-    this.nome = nome;
-    Animal.call(this, raca, idade);
-    
-    this.miar = function(){
-        console.log("Miau!")
-    }
-  }
-  
-  // Criando instâncias de objetos
-  const cachorro1 = new Cachorro("Rex", 3, "Labrador");
-  const gato1 = new Gato("Whiskers", 2, "Branco");
-  const cachorro2 = new Cachorro("Buddy", 5, "Golden Retriever");
-  
-  // Testando as instâncias
-  console.log(cachorro1);
-  cachorro1.latir();
-  
-  console.log(gato1);
-  gato1.miar();
-  
-  console.log(cachorro2);
-  cachorro2.latir();
-  
+
+const alunos = [
+  { nome: 'Alice', nota: 7 },
+  { nome: 'Bob', nota: 5 },
+  { nome: 'Carlos', nota: 8 },
+  { nome: 'Diana', nota: 4 },
+  { nome: 'Eva', nota: 9 },
+];
+
+function alunosAprovados(arrayDeAlunos) {
+  return arrayDeAlunos.filter(aluno => aluno.nota >= 6);
+}
+
+const alunosAprovadosArray = alunosAprovados(alunos);
+console.log(alunosAprovadosArray);
